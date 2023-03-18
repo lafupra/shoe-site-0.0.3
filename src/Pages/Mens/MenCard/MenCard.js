1d2572b130b312e3bb49d0fa05f688c3eab43b49
praful <cpraful966@gmail.com>
2023-03-18 20:58:23 +0530

    cart reactive

--- src/Pages/Mens/MenCard/MenCard.js
+++ src/Pages/Mens/MenCard/MenCard.js
@@ -7,30 +7,22 @@ const MenCard = ({item}) => {
   const handleLinkClick = () => {
 		window.scrollTo(0, 0);
 	  };
-
+console.log(item)
   return (
     <>
-  {!item ? <div className="product-list-card">
+ <div className="product-list-card">
+ 
   <div className="product-list-card-image">
- <img width="200px" height="200px" style={{backgroundColor:"gray"}} src="#" alt="Professional Shoe"/>
+  <Link to={`/singleproduct/${item._id}`} onClick={handleLinkClick}>  <img src={item.thumbnailimageUrl} alt="Professional Shoe"/> </Link>
   </div>
+  
   <div className="product-list-card-details">
-    <h3 className="product-list-card-title">name</h3>
-    <p className="product-list-card-price">₹ </p>
-    <p className="product-list-card-description"> </p>
-  <button className="product-list-card-button">Add to Cart</button>
-  </div>
-</div> : <div className="product-list-card">
-  <div className="product-list-card-image">
-  <Link to={`/singleproduct/${item._id}`} onClick={handleLinkClick}> <img width="200px" height="200px" src={item.thumbnailimageUrl} alt="Professional Shoe"/></Link>
-  </div>
-  <div className="product-list-card-details">
-    <h3 className="product-list-card-title">{item.name}</h3>
-    <p className="product-list-card-price">₹ {item.price}</p>
-    <p className="product-list-card-description"> {item.description}</p>
+    <h3 className="product-list-card-title">{item?.name}</h3>
+    <p className="product-list-card-price">₹ {item?.price}</p>
+    <p className="product-list-card-description"> {item?.description}</p>
     <Link to={`/singleproduct/${item._id}`} onClick={handleLinkClick}> <button className="product-list-card-button">Add to Cart</button></Link>
   </div>
-</div>} 
+</div>
     </>
   )
 }
