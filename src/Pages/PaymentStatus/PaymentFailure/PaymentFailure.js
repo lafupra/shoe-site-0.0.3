486cea4d89e08df820ccebd9c4bd6a1071411844
praful <cpraful966@gmail.com>
2023-03-28 14:29:45 +0530

    scss not used

--- src/Pages/PaymentStatus/PaymentFailure/PaymentFailure.js
+++ src/Pages/PaymentStatus/PaymentFailure/PaymentFailure.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./PaymentFailure.scss"
+// import "./PaymentFailure.scss"
 import {Link} from "react-router-dom"
 
 const PaymentFailure = () => {
--- src/Pages/PaymentStatus/PaymentSuccess/PaymentSuccess.js
+++ src/Pages/PaymentStatus/PaymentSuccess/PaymentSuccess.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./PaymentSuccess.scss"
+// import "./PaymentSuccess.scss"
 
 const PaymentSuccess = () => {
   return (
